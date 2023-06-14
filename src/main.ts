8130017d910fa83bbb3ecd08483d75d1701fbdf2
colaboradores_taktics <damianclluch@gmail.com>
2023-06-14 15:16:47 +0200

    add middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
+
+  // Middleware de logging
+  app.use((req, res, next) => {
+    console.log(`Received request: ${req.method} ${req.url}`);
+    next();
+  });
+
   await app.listen(3000);
 }
 bootstrap();
